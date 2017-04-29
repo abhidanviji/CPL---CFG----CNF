8498fcccff45072a53f5ee42bcea2f9bcced3424
abhidanviji <abhi_danviji@yahoo.co.in>
2017-04-29 02:20:27 -0400

    version 1.6

--- src/CFGtoCNF.java
+++ src/CFGtoCNF.java
@@ -1,57 +1,56 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CFGtoCNF {
 	public static ArrayList<String> l = new ArrayList<>();
 	public static Map<String, String> map = new LinkedHashMap<>();
-	
-	public static boolean chkCase(String alph){
+
+	public static boolean chkCase(String alph) {
 		return alph.equals(alph.toLowerCase());
 	}
-	
-	public static String cnf(String split){
-		String rhs="";
-		if(split.length()>2){
-			if(chkCase(split.substring(0, 1))){
-				rhs=rhs+"<"+split.substring(0, 1)+">";
+
+	public static String cnf(String split) {
+		String rhs = "";
+		if (split.length() > 2) {
+			if (chkCase(split.substring(0, 1))) {
+				rhs = rhs + "<" + split.substring(0, 1) + ">";
 				l.add(rhs);
 				map.put(rhs, split.substring(0, 1));
-			}else{
-				rhs=rhs+split.substring(0, 1);
+			} else {
+				rhs = rhs + split.substring(0, 1);
 			}
-			if(split.substring(1).length()>2){
-				rhs=rhs+"<"+split.substring(1)+">";
-				l.add("<"+split.substring(1)+">");
-				map.put("<"+split.substring(1)+">", cnf(split.substring(1)));
-			}else{
-				
-				split=split.substring(1).substring(0);
-				l.add("<"+split+">");
-				rhs=rhs+"<"+split+">";
-				map.put("<"+split+">", cnf(split));
-				
+			if (split.substring(1).length() > 2) {
+				rhs = rhs + "<" + split.substring(1) + ">";
+				l.add("<" + split.substring(1) + ">");
+				map.put("<" + split.substring(1) + ">", cnf(split.substring(1)));
+			} else {
+
+				split = split.substring(1).substring(0);
+				l.add("<" + split + ">");
+				rhs = rhs + "<" + split + ">";
+				map.put("<" + split + ">", cnf(split));
+
 			}
 			return rhs;
-			
-		}else{
-			if(chkCase(split)){
+
+		} else {
+			if (chkCase(split)) {
 				return split;
-			}else{
-				if(chkCase(split.substring(0,1))){
-					rhs=rhs+"<"+split.substring(0,1)+">";
-					l.add("<"+split.substring(0,1)+">");
-					map.put("<"+split.substring(0,1)+">", cnf(split.substring(0,1)));
-				}else{
-					rhs=rhs+split.substring(0,1);
+			} else {
+				if (chkCase(split.substring(0, 1))) {
+					rhs = rhs + "<" + split.substring(0, 1) + ">";
+					l.add("<" + split.substring(0, 1) + ">");
+					map.put("<" + split.substring(0, 1) + ">", cnf(split.substring(0, 1)));
+				} else {
+					rhs = rhs + split.substring(0, 1);
 				}
-				if(chkCase(split.substring(1))){
-					rhs=rhs+"<"+split.substring(1)+">";
+				if (chkCase(split.substring(1))) {
+					rhs = rhs + "<" + split.substring(1) + ">";
 					l.add(split.substring(1));
 					map.put(split.substring(1), cnf(split.substring(1)));
-				}else{
-					rhs=rhs+split.substring(1);
+				} else {
+					rhs = rhs + split.substring(1);
 				}
 			}
 			return rhs;
@@ -59,21 +58,15 @@ public class CFGtoCNF {
 	}
 
 	public static void main(String[] args) {
-		String cfg = "A -> ABceDFS";
-		
+		String cfg = "A -> aBcdE";
 		String split[] = cfg.split("->");
-		l.add(split[0]);
-		for(int i=0;i<split.length;i++){
-			split[i]=split[i].trim();
+		l.add(split[0].trim());
+		for (int i = 0; i < split.length; i++) {
+			split[i] = split[i].trim();
 		}
 		map.put(split[0], cnf(split[1]));
-		
-		
-		System.out.println(split[0]+"->"+map.get(split[0]));
-		for(int i=1;i<l.size();i++){
-			System.out.println(l.get(i)+"->"+map.get(l.get(i)));
+		for (int i = 0; i < l.size(); i++) {
+			System.out.println(l.get(i) + " -> " + map.get(l.get(i)));
 		}
-
 	}
-
 }
